15b5800d499cff0c1908bef1f4bbbcbc04ff7461
AndreiApopii <70907129+AndreiApopii@users.noreply.github.com>
2020-11-05 19:41:49 +0200

    Add files via upload

---
+++ problema4.py
@@ -0,0 +1,11 @@
+suma=0
+produsul=1
+numarul=1
+numarul=eval(input("n="))
+while numarul<=n:
+    suma+=numarul
+    produsul*=numarul
+    numarul+=1
+print("suma= ",suma)
+print("produsul= ",produsul)
+print("media aritmetica= ",suma/n)
